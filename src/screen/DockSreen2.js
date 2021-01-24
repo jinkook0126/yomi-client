2ee057a8ac00df4e6760369eafe6f7382d1096c5
jinqook <jinqook0126@gmail.com>
2021-01-24 23:18:50 +0900

    test icon position -ing

--- src/screen/DockSreen2.js
+++ src/screen/DockSreen2.js
@@ -22,12 +22,15 @@ const styles = StyleSheet.create({
     }
 });
 const fullWidth = Dimensions.get('window').width;
+const fullHeight = Dimensions.get('window').height;
 const windowHeight = (fullWidth*0.3) * 415 / 622;
 const deskHeight = (fullWidth*0.48) * 485 / 734;
 const bookCaseHeight = (fullWidth*0.33) * 847 / 510;
 const calcHeight = (fullWidth*0.27) * 296 / 431;
 const fridgeHeight = (fullWidth*0.3) * 837 / 460;
 const yomiHeight = (fullWidth*0.32) * 767 / 489;
+
+const halfPosition = (fullHeight*0.5);
 
 export default ()=>{
     return (
@@ -38,27 +41,27 @@ export default ()=>{
                 </View>
             </TouchableWithoutFeedback>
             <TouchableWithoutFeedback onPress={()=>{alert("desk")}}>
-                <View style={{position:"absolute",right:0,top:200,width:'48%',height:deskHeight}}>
+                <View style={{position:"absolute",right:0,top: halfPosition-deskHeight,width:'48%',height:deskHeight}}>
                     <Image source={require("../img/desk.png")} style={styles.window}/>
                 </View>
             </TouchableWithoutFeedback>
             <TouchableWithoutFeedback onPress={()=>{alert("calc")}}>
-                <View style={{position:"absolute",right:20,top:200-(calcHeight/2)-10,width:'27%',height:calcHeight}}>
+                <View style={{position:"absolute",right:20,top:(halfPosition-deskHeight)-(calcHeight/2)-10,width:'27%',height:calcHeight}}>
                     <Image source={require("../img/calc.png")} style={styles.window}/>
                 </View>
             </TouchableWithoutFeedback>
             <TouchableWithoutFeedback onPress={()=>{alert("bookcase")}}>
-                <View style={{position:"absolute",right:'48%',top:200+deskHeight-bookCaseHeight,width:'33%',height:bookCaseHeight}}>
+                <View style={{position:"absolute",right:'48%',top:halfPosition-bookCaseHeight,width:'33%',height:bookCaseHeight}}>
                     <Image source={require("../img/bookcase.png")} style={styles.window}/>
                 </View>
             </TouchableWithoutFeedback>
             <TouchableWithoutFeedback onPress={()=>{alert("fridge")}}>
-                <View style={{position:"absolute",left:0,top:240,width:'30%',height:fridgeHeight}}>
+                <View style={{position:"absolute",left:0,top:halfPosition-(fridgeHeight/2),width:'30%',height:fridgeHeight}}>
                     <Image source={require("../img/fridge.png")} style={styles.window}/>
                 </View>
             </TouchableWithoutFeedback>
             <TouchableWithoutFeedback onPress={()=>{alert("yomi")}}>
-                <View style={{position:"absolute",right:80,top:'55%',width:'32%',height:yomiHeight}}>
+                <View style={{position:"absolute",right:80,bottom:'10%',width:'32%',height:yomiHeight}}>
                     <Image source={require("../img/yomi.png")} style={styles.window}/>
                 </View>
             </TouchableWithoutFeedback>
