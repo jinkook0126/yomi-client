c98a1cb3fd1533736a8f08c431de6acdb0479030
jinqook <jinqook0126@gmail.com>
2021-01-24 22:02:50 +0900

    test room position -ing

--- src/screen/DockSreen2.js
+++ src/screen/DockSreen2.js
@@ -1,10 +1,97 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,Image,ImageBackground,StyleSheet,TouchableWithoutFeedback,Dimensions } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover', // or 'stretch'
+    },
+    wrap: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
+    },
+    window:{
+        width:"100%",
+        height:"100%"
+    }
+});
+const fullWidth = Dimensions.get('window').width;
+const windowHeight = (fullWidth*0.3) * 415 / 622;
+const deskHeight = (fullWidth*0.48) * 485 / 734;
+const bookCaseHeight = (fullWidth*0.33) * 847 / 510;
+const calcHeight = (fullWidth*0.27) * 296 / 431;
+const fridgeHeight = (fullWidth*0.3) * 837 / 460;
+const yomiHeight = (fullWidth*0.32) * 767 / 489;
 
 export default ()=>{
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor:'#b0e8d4' }}>
-            <Text>Dock Screen 2 입니다!!!</Text>
-        </View>
+        <ImageBackground source={require("../img/home_bg.png")} style={{width: '100%', height: '100%'}}>
+            <TouchableWithoutFeedback onPress={()=>{alert("window!")}}>
+                <View style={{position:"absolute",right:20,top:20,width:'30%',height:windowHeight}}>
+                    <Image source={require("../img/window.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{alert("desk")}}>
+                <View style={{position:"absolute",right:0,top:200,width:'48%',height:deskHeight}}>
+                    <Image source={require("../img/desk.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{alert("calc")}}>
+                <View style={{position:"absolute",right:20,top:200-(calcHeight/2)-10,width:'27%',height:calcHeight}}>
+                    <Image source={require("../img/calc.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{alert("bookcase")}}>
+                <View style={{position:"absolute",right:'48%',top:200+deskHeight-bookCaseHeight,width:'33%',height:bookCaseHeight}}>
+                    <Image source={require("../img/bookcase.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{alert("fridge")}}>
+                <View style={{position:"absolute",left:0,top:240,width:'30%',height:fridgeHeight}}>
+                    <Image source={require("../img/fridge.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{alert("yomi")}}>
+                <View style={{position:"absolute",right:80,top:'55%',width:'32%',height:yomiHeight}}>
+                    <Image source={require("../img/yomi.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback>
+            {/* <TouchableWithoutFeedback onPress={()=>{alert("window!")}}>
+                <View style={{position:"absolute",right:20,top:20,width:150,height:100}}>
+                    <Image source={require("../img/window.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{alert("desk")}}>
+                <View style={{position:"absolute",right:0,top:200,width:182,height:120}}>
+                    <Image source={require("../img/desk.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{alert("bookcase")}}>
+                <View style={{position:"absolute",right:182,top:104,width:130,height:216}}>
+                    <Image source={require("../img/bookcase.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{alert("calc")}}>
+                <View style={{position:"absolute",right:20,top:200-40,width:91,height:62}}>
+                    <Image source={require("../img/calc.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{alert("fridge")}}>
+                <View style={{position:"absolute",left:0,top:200,width:120,height:218}}>
+                    <Image source={require("../img/fridge.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{alert("yomi")}}>
+                <View style={{position:"absolute",right:80,top:'50%',width:120,height:188}}>
+                    <Image source={require("../img/yomi.png")} style={styles.window}/>
+                </View>
+            </TouchableWithoutFeedback> */}
+        </ImageBackground>
     )
 }
