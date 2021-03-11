ef97f82f9ca4909afe2c7df5fdc74b7842e31d4b
jinqook <jinqook0126@gmail.com>
2021-03-12 02:07:43 +0900

    star rating -ing

--- src/modal/BookModalContents.js
+++ src/modal/BookModalContents.js
@@ -1,61 +1,49 @@
 import React,{useState} from 'react';
-import { TextInput,Text,View,Button,FlatList,Image } from 'react-native';
+import { TextInput,Text,View,Button,FlatList,Image,StyleSheet } from 'react-native';
+import { Rating } from 'react-native-ratings';
 import { useDispatch } from 'react-redux';
 import {closeModal} from '../reducers/modal';
 import axios from 'axios';
 export default ()=>{
     const dispatch = useDispatch();
-    const API_KEY = "a5bf620feebcc2c9bf7af09e3eb5d39d";
-    const [bookSelect,setBookSelect] = useState([]);
-    const [bookTitle,setBookTitle] = useState("");
-    const searchBooks = async()=>{
-        let emtpyArr = [];
-        const {data} = await axios.get("https://dapi.kakao.com/v3/search/book?target=title",{
-            params:{
-                query:bookTitle
-            },
-            headers:{
-                Authorization:`KakaoAK ${API_KEY}`
-            }
-        });
-        for (const {authors, title, thumbnail,isbn} of data.documents) {
-            emtpyArr.push({
-                authors:authors[0],
-                title:title,
-                thumbnail:thumbnail,
-                isbn:isbn
-            })
-        }
-        setBookSelect(emtpyArr);
-    };
-    const renderItem = ({item}) =>{
-        const imgUrl = item.thumbnail !== "" ? {uri:item.thumbnail} : require('../img/emptyThumbnail.png')
-        return (
-            <View style={{borderBottomWidth:1,padding:3}}>
-                <Image source={imgUrl} style={{width:40,height:40,resizeMode:"contain"}}/>
-                <Text>{item.title}</Text>
-                <Text>{item.authors}</Text>
-            </View>
-        )
-    }
     return (
-        <View style={{width:300,paddingHorizontal:12, paddingVertical:8,backgroundColor:'white',height:400}}>
-
-            <View style={{paddingVertical:8}}>
-                <TextInput placeholder="책 검색" onChangeText={text=>setBookTitle(text)} value={bookTitle} style={{borderWidth:1,borderColor:'grey'}} />
-            </View>
-            <View style={{paddingVertical:8}}>
-                <Button title="search" onPress={searchBooks}/>
-            </View>
-            <FlatList 
-                data={bookSelect}
-                renderItem={renderItem}
-                keyExtractor={item=> item.isbn}
-                style={{height:"50%",borderWidth:1,borderColor:'red',flexGrow:0}}
-            />
-            <View style={{paddingTop:30}}>
-                <Button title="MDOAL CLOSE!!" onPress={()=>dispatch(closeModal())}></Button>
+        <View style={{width:300,padding:16,backgroundColor:'white',borderRadius:8}}>
+            <View>
+                <View style={{flexDirection:'row',borderBottomWidth:1,borderBottomColor:"#EEEEEE",paddingBottom:16}}>
+                    <Image source={require('../img/emptyThumbnail.png')} style={{width:67,height:84}}/>
+                    <View style={{paddingLeft:4,flex:1,height:84,overflow:"hidden"}}>
+                        <Text style={[styles.commonColor,{fontSize:14,fontWeight:'bold'}]}>멋진 신세계</Text>
+                        <Text style={{fontSize:10,color:"#757575"}}>이진국짱이다.</Text>
+                        <Text numberOfLines={4} ellipsizeMode={"tail"} style={{fontSize:8,color:'#757575',lineHeight:11}}>한 난자에서 180가지의 인간을 생산해내는 공장과 그 아이들을 타율과 강제에 의해 주어진 조건 속에서 교육, 훈련시키는 장면으로 『멋진 신세계』는 시작된다. </Text>
+                    </View>
+                </View>
+                <View style={{marginTop:25,backgroundColor:'green',paddingHorizontal:5,flexDirection:'row',alignItems:'center'}}>
+                    <Text style={{fontSize:12,color:"#424242",fontWeight:'bold'}}>읽은 페이지 수</Text>
+                    <View style={{backgroundColor:"#EEEEEE",width:22,height:16,marginLeft:10}}>
+                        <TextInput style={{flex:1,height:16,alignItems:"stretch",paddingVertical:0,fontSize:12}}/>
+                    </View>
+                </View>
+                <View style={{marginTop:10,backgroundColor:'blue',paddingHorizontal:5,flexDirection:'row',alignItems:'center'}}>
+                    <Text style={{fontSize:12,color:"#424242",fontWeight:'bold'}}>별점</Text>
+                    <Rating
+                        fractions={2}
+                        ratingCount={5}
+                        minValue={0.5}
+                        jumpValue={0.5}
+                        imageSize={14}
+                        style={{marginLeft:16,backgroundColor:'red'}}
+                    />
+                </View>
             </View>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    commonColor: {
+        color:"#2B2B2B"
+    },
+
+    listContainer:{
+        elevation:1
+    }
+});
