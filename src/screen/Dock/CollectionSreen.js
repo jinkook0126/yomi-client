f81775af06d85d07cab404f5437a8d5d2068d662
jinqook <jinqook0126@gmail.com>
2021-03-25 01:54:49 +0900

    collection screen -ing

--- src/screen/Dock/CollectionSreen.js
+++ src/screen/Dock/CollectionSreen.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { Text, View,Button } from 'react-native';
+import { Text, View,Button,SafeAreaView,Image,TouchableOpacity } from 'react-native';
 import { useDispatch } from 'react-redux';
 import {openModal} from '../../reducers/modal';
 
+const img_prefix = "https://yomi-image.s3.ap-northeast-2.amazonaws.com";
 export default ()=>{
     const dispatch = useDispatch();
     const openModalTest=(target)=>{
@@ -10,20 +11,39 @@ export default ()=>{
     }
     
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor:'#b9e450' }}>
-            <Text>Dock Screen 1 입니다!!!</Text>
-            <View style={{paddingVertical:10}}>
-                <Button title="TEST 모달" onPress={()=>openModalTest("test")} ></Button>
+        <SafeAreaView style={{flex:1,backgroundColor:"#FFFFFF"}}>
+            <View style={{height:66,flexDirection:"row",padding:20,alignItems:"center"}}>
+                <Image source={require('../../img/ico_symbol.png')}/>
+                <Text style={{fontSize:16,fontWeight:"bold",color:"#000000",marginLeft:14}}>2 coin</Text>
             </View>
-            <Viewrr style={{paddingVertical:10}}>
-                <Button title="책 모달" onPress={()=>openModalTest("book")} color={"pink"}></Button>
-            </Viewrr>
-            <View style={{paddingVertical:10}}>
-                <Button title="공부 모달" onPress={()=>openModalTest("desk")} color={"grey"}></Button>
+            <View style={{height:4,backgroundColor:'#EEEEEE'}}>
             </View>
-            <View style={{paddingVertical:10}}>
-                <Button title="운동 모달" onPress={()=>openModalTest("health")} color={"cyan"}></Button>
+            <View style={{padding:20}}>
+                <View style={{flexDirection:'row',alignItems:'center',justifyContent:"space-between"}}>
+                    <TouchableOpacity>
+                        <Text style={{fontWeight:'bold',color:'#616161',fontSize:15}}>냉장고</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={{fontWeight:'bold',color:'#000000',fontSize:15}}>책상</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={{fontWeight:'bold',color:'#616161',fontSize:15}}>운동기구</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={{fontWeight:'bold',color:'#616161',fontSize:15}}>일기장</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={{fontWeight:'bold',color:'#616161',fontSize:15}}>책장</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={{paddingVertical:16,marginTop:30,elevation:2,backgroundColor:"#FFFFFF",alignItems:"center"}}>
+                    <Text style={{fontSize:14,fontWeight:'bold',color:"#000000"}}>미리보기</Text>
+                    <Image source = {{ uri: 'https://yomi-image.s3.ap-northeast-2.amazonaws.com/desk/desk01.png'}}  style={{width:112,height:80,resizeMode:'stretch',marginTop:25}}/>
+                </View>
+                <View style={{marginTop:20,elevation:2,backgroundColor:"#FFFFFF"}}>
+                    
+                </View>
             </View>
-        </View>
+        </SafeAreaView>
     )
 }
