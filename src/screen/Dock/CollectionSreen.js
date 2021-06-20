d93db181bfa7ad2505fec45099f98f0ea635782c
jinqook <jinqook0126@gmail.com>
2021-06-20 23:51:43 +0900

    collection api -fin

--- src/screen/Dock/CollectionSreen.js
+++ src/screen/Dock/CollectionSreen.js
@@ -8,48 +8,83 @@ const img_prefix = "https://yomi-image.s3.ap-northeast-2.amazonaws.com";
 export default ()=>{
     const dispatch = useDispatch();
     const [itemList,setItemList] = useState([]);
-    const [selectCt,setSelectCt] = useState('FT02')
+    const [selectCt,setSelectCt] = useState('FT02');
+    const [previewUri,setPreviewUri] = useState("");
+    const [coin,setCoin] = useState(0);
 
     useEffect(()=>{
         renderCategory('FT02');
     },[]);
 
     const renderCategory = async (code)=> {
-        const {success,lists} = await send.get("/collection",{params:{category:code}});
-        setItemList(lists)
-    }
-
-    const openModalTest=(target)=>{
-        dispatch(openModal(target));
+        const {success,lists,coin:dbCoin} = await send.get("/collection",{params:{category:code}});
+        if(success){
+            setItemList(lists);
+            setCoin(dbCoin);
+        }
     }
 
     const handleCategory = (ct) =>{
         renderCategory(ct);
         setSelectCt(ct);
+        setPreviewUri("");
     }
     
     const selectHeader = (ct) => (
         selectCt === ct ? "#000000" : '#616161'
     )
 
+    const handlePreview = (uri) =>{
+        setPreviewUri(uri)
+    }
+
+    const buyItem = async(code,price) => {
+        if(price > coin) {
+            alert('코인이 부족합니다.')
+        } else {
+            const {success,coin:dbCoin,category} = await send.post("/collection/buy",{id:code});
+            if(success) {
+                alert("구매하였습니다.");
+                setCoin(dbCoin);
+                renderCategory(category);
+            }
+        }
+    }
+
+    const activeItem = async(code) => {
+        const {success,msg} = await send.put("/collection/active",{id:code});
+        if(success) {
+            alert("적용하였습니다.");
+        } else {
+            alert(msg);
+        }
+    }
+
 
     const renderItem = ({item,index})=>{
         return (
             <View style={{alignItems:"center"}}>
-                <Image source={{ uri: img_prefix+item.FT_URL}} style={{width:57,height:47,resizeMode:'stretch',justifyContent:"space-around"}}/>
+                <TouchableOpacity onPress={()=>handlePreview(item.FT_URL)}>
+                    <Image source={{ uri: img_prefix+item.FT_URL }} style={{width:57,height:47,resizeMode:'stretch',justifyContent:"space-around"}}/>
+                </TouchableOpacity>
                 <Text style={{fontSize:12,color:"#000000",fontWeight:'bold',marginTop:14}}>{item.FT_PRICE} coin</Text>
                 {
-                    item.have ? 
+                    item.has ? 
                     (
-                        <View style={{marginTop:12,borderRadius:6,width:60,height:28,backgroundColor:"#8C6C51",justifyContent:'center',alignItems:"center"}}>
-                            <Text style={{fontSize:11,color:"#FFFFFF",fontWeight:'bold'}}>보유중</Text>
-                        </View>
+                        <TouchableOpacity onPress={()=>activeItem(item.FT_ID)}>
+                            <View style={{marginTop:12,borderRadius:6,width:60,height:28,backgroundColor:"#8C6C51",justifyContent:'center',alignItems:"center"}}>
+                                <Text style={{fontSize:11,color:"#FFFFFF",fontWeight:'bold'}}>적용하기</Text>
+                            </View>
+                        </TouchableOpacity>
+                        
                     )
                     :
                     (
+                        <TouchableOpacity onPress={()=>buyItem(item.FT_ID,item.FT_PRICE)}>
                         <View style={{marginTop:12,borderRadius:6,width:60,height:28,backgroundColor:"#7A6FE5",justifyContent:'center',alignItems:"center"}}>
-                            <Text style={{fontSize:11,color:"#FFFFFF",fontWeight:'bold'}}>구매하기</Text>
-                        </View>
+                                <Text style={{fontSize:11,color:"#FFFFFF",fontWeight:'bold'}}>구매하기</Text>
+                            </View>
+                        </TouchableOpacity>
                     )
                 }
             </View>
@@ -60,7 +95,7 @@ export default ()=>{
         <SafeAreaView style={{flex:1,backgroundColor:"#FFFFFF"}}>
             <View style={{height:66,flexDirection:"row",padding:20,alignItems:"center"}}>
                 <Image source={require('../../img/ico_symbol.png')}/>
-                <Text style={{fontSize:16,fontWeight:"bold",color:"#000000",marginLeft:14}}>2 coin</Text>
+                <Text style={{fontSize:16,fontWeight:"bold",color:"#000000",marginLeft:14}}>{coin} coin</Text>
             </View>
             <View style={{height:4,backgroundColor:'#EEEEEE'}}>
             </View>
@@ -84,7 +119,12 @@ export default ()=>{
                 </View>
                 <View style={{paddingVertical:16,marginTop:30,elevation:2,backgroundColor:"#FFFFFF",alignItems:"center"}}>
                     <Text style={{fontSize:14,fontWeight:'bold',color:"#000000"}}>미리보기</Text>
-                    <Image source = {{ uri: img_prefix+'/desk/desk01.png'}}  style={{width:112,height:80,resizeMode:'stretch',marginTop:25}}/>
+                    {
+                        previewUri !== "" ?
+                        <Image source = {{ uri: img_prefix+previewUri}}  style={{width:112,height:80,resizeMode:'stretch',marginTop:25}}/>
+                        :
+                        <View style={{height:80,marginTop:25}}></View>
+                    }
                 </View>
                 <View style={{marginTop:20,elevation:2,backgroundColor:"#FFFFFF",paddingVertical:20,alignItems:'center'}}>
                     <Text style={{fontSize:14,fontWeight:'bold',color:"#000000"}}>구매</Text>
